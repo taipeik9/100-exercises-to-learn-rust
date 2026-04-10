700d512788dbcadd7c89c6877921dc0d92929dbd
Henry Brown <henry.jy.brown@gmail.com>
2026-04-10 16:05:29 -0400

    done 02/07

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,17 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    if n == 0 {
+        return 1;
+    }
+    let mut total: u32 = 1;
+    let mut i: u32 = n;
+
+    while i > 1 {
+        total *= i;
+        i -= 1;
+    }
+    total
 }
 
 #[cfg(test)]
